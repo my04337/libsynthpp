8027f0d9c054eb8d3bb38c472784ac5ad1d2f595
my04337 <miuexa@gmail.com>
2018-05-22 14:33:37 +0900

    Luath : ToneMapper ノートOn/Off管理+ダンパーペダル対応

---
+++ include/LSP/MIDI/Synthesizer/ToneMapper.hpp
@@ -0,0 +1,57 @@
+﻿#pragma once
+
+#include <LSP/Base/Base.hpp>
+#include <LSP/Base/Id.hpp>
+
+namespace LSP::MIDI::Synthesizer
+{
+
+struct _tone_id_tag {};
+using ToneId = issuable_id_base_t<_tone_id_tag>;
+
+// ノートOn/Off情報から各トーンへマッピングする
+class ToneMapper
+	: non_copy
+{
+public:
+	using Callback = std::function<void(ToneId toneNo, uint32_t noteNo, uint8_t vel)>;
+
+public:
+	void setCallback(Callback callback);
+
+	// 発音状態をリセットします (コールバックは無し)
+	void reset();
+
+	// 同時発音数を取得します
+	size_t count()const noexcept;
+
+	// ノート オン (vel > 0)
+	ToneId noteOn(uint32_t noteNo, uint8_t vel);
+	// ノート オフ
+	void noteOff(uint32_t noteNo, bool force=false);
+	// ダンパーペダル オン
+	void holdOn();
+	// ダンパーペダル オフ
+	void holdOff();
+	
+private:
+	struct NoteInfo {
+		ToneId toneId;			// トーンId
+		bool holding = false;	// ノートオフ後でもダンパーペダルによって保持されている
+	};
+	using ToneMap = std::unordered_map<uint32_t, NoteInfo>;
+
+	ToneId _noteOn(uint32_t noteNo, uint8_t vel);
+	void _noteOff(uint32_t noteNo);
+	ToneMap::iterator _noteOff(ToneMap::iterator iter);
+	void _setHold(uint32_t noteNo);
+	void _resetHold();
+
+private:
+
+	Callback mCallback;
+	ToneMap mTones;
+	bool mHold=false; // ダンパーペダル 状態
+};
+
+}
---
+++ src/MIDI/Synthesizer/ToneMapper.cpp
@@ -0,0 +1,84 @@
+﻿#include <LSP/MIDI/Synthesizer/ToneMapper.hpp>
+#include <LSP/Base/Logging.hpp>
+
+using namespace LSP;
+using LSP::MIDI::Synthesizer::ToneId;
+using LSP::MIDI::Synthesizer::ToneMapper;
+
+
+void ToneMapper::setCallback(Callback callback) 
+{
+	mCallback = std::move(callback); 
+}
+
+void ToneMapper::reset()
+{
+	mTones.clear();
+	mHold = false;
+}
+size_t ToneMapper::count()const noexcept
+{
+	return mTones.size();
+}
+
+ToneId ToneMapper::noteOn(uint32_t noteNo, uint8_t vel)
+{
+	// MEMO : 同じノート番号は同時に発音不可
+	_noteOff(noteNo);
+	return _noteOn(noteNo, vel);
+}
+void ToneMapper::noteOff(uint32_t noteNo, bool force)
+{
+	if (!mHold || force) {
+		_noteOff(noteNo);
+	} else {
+		_setHold(noteNo);
+	}
+}
+void ToneMapper::holdOn()
+{
+	mHold = true;
+}
+void ToneMapper::holdOff()
+{
+	_resetHold();
+}
+ToneId ToneMapper::_noteOn(uint32_t noteNo, uint8_t vel)
+{
+	lsp_assert(vel > 0);
+	auto id = ToneId::issue();
+	mTones[noteNo] = NoteInfo{id, false};
+	if(mCallback) mCallback(id, noteNo, vel);
+	return id;
+}
+void ToneMapper::_noteOff(uint32_t noteNo)
+{
+	auto found = mTones.find(noteNo);
+	if(found == mTones.end()) return;
+	_noteOff(found);
+}
+ToneMapper::ToneMap::iterator ToneMapper::_noteOff(ToneMapper::ToneMap::iterator iter)
+{
+	auto noteNo = iter->first;
+	auto id = iter->second.toneId;
+	auto retIter = mTones.erase(iter);
+	if(mCallback) mCallback(id, noteNo, 0);
+	return retIter;
+}
+void ToneMapper::_setHold(uint32_t noteNo)
+{
+	auto found = mTones.find(noteNo);
+	if(found == mTones.end()) return;
+
+	found->second.holding = true;
+}
+void ToneMapper::_resetHold()
+{
+	for (auto iter = mTones.begin(); iter != mTones.end();) {
+		if (iter->second.holding) {
+			iter = _noteOff(iter);
+		} else {
+			++iter;
+		}
+	}
+}
