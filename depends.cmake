02e62805d6b6b583f3ed134494f8b19df3ebd468
my04337 <miuexa@gmail.com>
2023-06-19 16:58:08 +0900

    third_patry : SDL, SDL_ttf のビルドに用いたスクリプトのメモを追加

--- depends.cmake
+++ depends.cmake
@@ -6,6 +6,20 @@ include(FetchContent)
 ### SDL2
 unset(SDL2_FIND_PACKAGE_EXTRA_ARGS)
 if(MSVC)
+	#[[
+		set WORK_PATH=C:\build
+		set LIBRARY_NAME=SDL2
+		set LIBRARY_URL=https://github.com/libsdl-org/SDL
+		set LIBRARY_TAG=release-2.26.5
+
+		cd %WORK_PATH%
+		mkdir %LIBRARY_NAME%-build
+		git clone %LIBRARY_URL% -b %LIBRARY_TAG% %LIBRARY_NAME%-src
+
+		cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%WORK_PATH%\%LIBRARY_NAME%  -S %LIBRARY_NAME%-src -B%LIBRARY_NAME%-build
+		cmake --build %LIBRARY_NAME%-build
+		cmake --install %LIBRARY_NAME%-build
+	]]
 	set(SDL2_PREBUILT_SUFFIX "2.26.5_win32_msvc2022_x64")
 	set(SDL2_PREBUILT_DIR ${CMAKE_BINARY_DIR}/_deps/SDL2-${SDL2_PREBUILT_SUFFIX})
 	if(NOT EXISTS ${SDL2_PREBUILT_DIR})
@@ -24,6 +38,20 @@ find_package(SDL2 REQUIRED ${SDL2_FIND_PACKAGE_EXTRA_ARGS})
 ### SDL2_ttf
 unset(SDL2_ttf_FIND_PACKAGE_EXTRA_ARGS)
 if(MSVC)
+	#[[
+		set WORK_PATH=C:\build
+		set LIBRARY_NAME=SDL2_ttf
+		set LIBRARY_URL=https://github.com/libsdl-org/SDL_ttf
+		set LIBRARY_TAG=release-2.20.2
+
+		cd %WORK_PATH%
+		mkdir %LIBRARY_NAME%-build
+		git clone %LIBRARY_URL% --recursive -b %LIBRARY_TAG% %LIBRARY_NAME%-src
+
+		cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%WORK_PATH%\SDL2 -DCMAKE_INSTALL_PREFIX=%WORK_PATH%\%LIBRARY_NAME%  -S %LIBRARY_NAME%-src -B%LIBRARY_NAME%-build
+		cmake --build %LIBRARY_NAME%-build
+		cmake --install %LIBRARY_NAME%-build
+	]]
 	set(SDL2_ttf_PREBUILT_SUFFIX "2.20.2_win32_msvc2022_x64")
 	set(SDL2_ttf_PREBUILT_DIR ${CMAKE_BINARY_DIR}/_deps/SDL2_ttf-${SDL2_ttf_PREBUILT_SUFFIX})
 	if(NOT EXISTS ${SDL2_ttf_PREBUILT_DIR})
