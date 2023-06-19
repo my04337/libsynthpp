c238aa17e7e58bef463aa03c6fb72e4d8a32a5d8
my04337 <miuexa@gmail.com>
2023-06-19 14:09:57 +0900

    third_party : SDL2, SDL2_ttf のビルド済バイナリ同梱 ※将来的にビルドオプションをカスタマイズする可能性があるため

--- depends.cmake
+++ depends.cmake
@@ -4,27 +4,35 @@ include(FetchContent)
 
 ###########################################################
 ### SDL2
-FetchContent_Declare(
-	sdl2	
-	URL https://www.libsdl.org/release/SDL2-devel-2.26.5-VC.zip
-	URL_HASH SHA1=f040c352af677161200ec07463efe8d1325135e4
-)
-FetchContent_MakeAvailable(sdl2)
-find_package(
-	SDL2 REQUIRED
-	HINTS ${sdl2_SOURCE_DIR}
-)
+unset(SDL2_FIND_PACKAGE_EXTRA_ARGS)
+if(MSVC)
+	set(SDL2_PREBUILT_SUFFIX "2.26.5_win32_msvc2022_x64")
+	set(SDL2_PREBUILT_DIR ${CMAKE_BINARY_DIR}/_deps/SDL2-${SDL2_PREBUILT_SUFFIX})
+	if(NOT EXISTS ${SDL2_PREBUILT_DIR})
+		message(STATUS "Extracting prebuilt binary : SDL-${SDL2_PREBUILT_SUFFIX}")
+		file(ARCHIVE_EXTRACT 
+			INPUT ${CMAKE_CURRENT_LIST_DIR}/third_party/SDL2/SDL2-${SDL2_PREBUILT_SUFFIX}.7z
+			DESTINATION ${SDL2_PREBUILT_DIR}
+		)
+	endif()
+	set(SDL2_FIND_PACKAGE_EXTRA_ARGS HINTS ${SDL2_PREBUILT_DIR})
+endif()
+find_package(SDL2 REQUIRED ${SDL2_FIND_PACKAGE_EXTRA_ARGS})
 
 
 ###########################################################
 ### SDL2_ttf
-FetchContent_Declare(
-	sdl2_ttf	
-	URL https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.20.2-VC.zip
-	URL_HASH SHA1=dee48e9c5184c139aa8bcab34a937d1b3df4f503
-)
-FetchContent_MakeAvailable(sdl2_ttf)
-find_package(
-	SDL2_ttf REQUIRED
-	HINTS ${sdl2_ttf_SOURCE_DIR}
-)
+unset(SDL2_ttf_FIND_PACKAGE_EXTRA_ARGS)
+if(MSVC)
+	set(SDL2_ttf_PREBUILT_SUFFIX "2.20.2_win32_msvc2022_x64")
+	set(SDL2_ttf_PREBUILT_DIR ${CMAKE_BINARY_DIR}/_deps/SDL2_ttf-${SDL2_ttf_PREBUILT_SUFFIX})
+	if(NOT EXISTS ${SDL2_ttf_PREBUILT_DIR})
+		message(STATUS "Extracting prebuilt binary : SDL_ttf-${SDL2_ttf_PREBUILT_SUFFIX}")
+		file(ARCHIVE_EXTRACT 
+			INPUT ${CMAKE_CURRENT_LIST_DIR}/third_party/SDL2_ttf/SDL2_ttf-${SDL2_ttf_PREBUILT_SUFFIX}.7z
+			DESTINATION ${SDL2_ttf_PREBUILT_DIR}
+		)
+	endif()
+	set(SDL2_ttf_FIND_PACKAGE_EXTRA_ARGS HINTS ${SDL2_ttf_PREBUILT_DIR})
+endif()
+find_package(SDL2_ttf REQUIRED ${SDL2_ttf_FIND_PACKAGE_EXTRA_ARGS})
