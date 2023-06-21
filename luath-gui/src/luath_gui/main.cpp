d4da9e050fb242d5c820bfd334186c9f5cbe434e
my04337 <miuexa@gmail.com>
2023-06-21 17:01:35 +0900

    SDL廃止準備 : WinMain置換OK

--- luath-gui/src/luath_gui/main.cpp
+++ luath-gui/src/luath_gui/main.cpp
@@ -1,18 +1,25 @@
 ﻿#include <luath_gui/app/application.hpp>
 
+using namespace lsp;
+using namespace luath_gui;
 
-int main(int argc, char** argv)
+int WINAPI WinMain(
+	HINSTANCE /* hInstance */,
+	HINSTANCE /* hPrevInstance */,
+	LPSTR /* lpCmdLine */,
+	int /* nCmdShow */
+)
 {
 	// ログ出力機構 セットアップ
-#ifdef WIN32
-	lsp::OutputDebugStringLogger logger;
-#else
-	StdOutLogger logger;
-#endif
-	lsp::Log::addLogger(&logger);
-	auto fin_act_logger = lsp::finally([&]{ lsp::Log::removeLogger(&logger); });
-	lsp::Log::setLogLevel(lsp::LogLevel::Debug);
-	
-	// スタート
-	return luath_gui::Application::instance().exec(argc, argv);
+	OutputDebugStringLogger logger;
+	Log::addLogger(&logger);
+	auto fin_act_logger = finally([&] { lsp::Log::removeLogger(&logger); });
+	Log::setLogLevel(lsp::LogLevel::Debug);
+
+	// COM初期化
+	check(SUCCEEDED(CoInitialize(NULL)));
+	auto fin_act_com = finally([] {CoUninitialize(); });
+
+	// アプリケーション起動
+	return Application::instance().exec(__argc, __argv);
 }
