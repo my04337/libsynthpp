9a3e61c7a2193f5ab3bc2c2fd1bd47dcc896809d
my04337 <miuexa@gmail.com>
2023-06-29 23:13:05 +0900

    third_paty : JUCE - git submodule が安定しないので FetchContentに切替

--- depends.cmake
+++ depends.cmake
@@ -1,6 +1,5 @@
 ﻿cmake_minimum_required(VERSION 3.26)
 
-include(ExternalProject)
 include(FetchContent)
 
 ###########################################################
@@ -19,6 +18,13 @@ set(JUCE_BUILD_EXAMPLES NO)
 
 set(JUCE_SOURCE_DIR  "${CMAKE_CURRENT_LIST_DIR}/third_party/JUCE")
 set(JUCE_MODULES_DIR "${JUCE_SOURCE_DIR}/modules")
+
+FetchContent_Populate(
+    juce
+    GIT_REPOSITORY  https://github.com/juce-framework/JUCE
+    GIT_TAG         69795dc8e589a9eb5df251b6dd994859bf7b3fab # 7.0.5
+    SOURCE_DIR      ${CMAKE_CURRENT_LIST_DIR}/third_party/JUCE
+)
 
 include("${JUCE_SOURCE_DIR}/extras/Build/CMake/JUCEModuleSupport.cmake")
 
