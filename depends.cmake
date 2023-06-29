e757fb7c69be92d8b5983c75e9c3a32aa01ef412
my04337 <miuexa@gmail.com>
2023-06-29 14:19:58 +0900

    third_party : JUCE 7.0.5

--- depends.cmake
+++ depends.cmake
@@ -8,8 +8,8 @@ include(FetchContent)
 # 
 # 注意 : ISC Licenseで提供されるモジュールのみ利用可能
 #      * juce_core
-#      * juce_audio_devices
 #      * juce_audio_basics
+#      * juce_audio_devices
 #      * juce_events
 #        
 
@@ -17,4 +17,15 @@ set(JUCE_MODULES_ONLY YES)
 set(JUCE_BUILD_EXTRAS NO)
 set(JUCE_BUILD_EXAMPLES NO)
 
-add_subdirectory(third_party/JUCE)
+set(JUCE_SOURCE_DIR  "${CMAKE_CURRENT_LIST_DIR}/third_party/JUCE")
+set(JUCE_MODULES_DIR "${JUCE_SOURCE_DIR}/modules")
+
+include("${JUCE_SOURCE_DIR}/extras/Build/CMake/JUCEModuleSupport.cmake")
+
+juce_add_modules(
+    ALIAS_NAMESPACE juce
+    "${JUCE_MODULES_DIR}/juce_core"
+    "${JUCE_MODULES_DIR}/juce_audio_basics"
+    "${JUCE_MODULES_DIR}/juce_audio_devices"
+    "${JUCE_MODULES_DIR}/juce_events"
+)
